b6c6f10f6778eefda7d62fcefd1a6f2d166e1f15
Quang Phan <121747215+quangdev1607@users.noreply.github.com>
2025-04-26 10:47:43 +0700

    improve dx exp

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -12,9 +12,9 @@ function App() {
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
-            <h1>Vite + React</h1>
+            <h1>Hello Electron</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <button onClick={() => setCount((count) => count + 1)}>count number is {count}</button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
