3f226585385cf28d2c00fdffb246271e41925edd
Quang Phan <121747215+quangdev1607@users.noreply.github.com>
2025-04-26 16:03:03 +0700

    downgrade packages version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: "./",
     plugins: [react()],
+    base: "./",
     build: {
         outDir: "dist-react",
     },
